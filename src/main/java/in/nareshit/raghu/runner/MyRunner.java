325b7e7b69a50a38b7c9ecd9ba8d333507a44b84
Your Name <you@example.com>
2020-07-26 19:41:33 +0530

    US for changing MyRunner

--- src/main/java/in/nareshit/raghu/runner/MyRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MyRunner.java
@@ -1,13 +1,26 @@
 package in.nareshit.raghu.runner;
 
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MyRunner implements CommandLineRunner {
 
+	private static org.slf4j.Logger log = LoggerFactory.getLogger(MyRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("MyRunner class testing for git hub");
+		int a=10;
+		try
+		{
+		log.info("started");
+		System.out.println("MyRunner class testing for git hub"+a);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.info("Ented");
 
 	}
 
